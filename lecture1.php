669cae481355d337d9af36c652c76b9c7dc99512
Marko Markovic <activis95@yahoo.com>
2023-11-07 13:33:28 +0100

    Finishing few chapters

--- lecture1.php
+++ lecture1.php
@@ -7,6 +7,23 @@
 <body>
 	<?php
         echo "My first PHP script!";
+
+        # Case sensitive
+
+        $color = "red";
+        echo "My car is " . $color . "<br>";
+        echo "My house is " . $COLOR . "<br>";
+        echo "My boat is " . $coLOR . "<br>";
+
+        # Single line comment
+        // Single line comment
+        /*
+        Multi line comment
+        like this
+        */
+
+
+        #Variable always start with $
     ?>
 </body>
 </html>
