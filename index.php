e9338049344b0f4e27f1af087530a639927f98cb
Marko Markovic <activis95@yahoo.com>
2023-11-07 13:24:21 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Lectures</title>
+</head>
+<body>
+    <h1>Welcome to PHP Lectures</h1>
+    <p>Select a lecture from below:</p>
+    <ul>
+        <li><a href="lecture1.php">Lecture 1</a></li>
+        <li><a href="lecture2.php">Lecture 2</a></li>
+        <!-- Add more lectures as you create them -->
+    </ul>
+</body>
+</html>
